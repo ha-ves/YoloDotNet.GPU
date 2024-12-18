8fc39c09dc483e70e34fdcca2dcbfe13bb40be6a
Haves Irfan <20160532+ha-ves@users.noreply.github.com>
2024-12-18 22:56:43 +0900

    Add project files.

---
+++ DirectX/Core/YoloCoreDirectX.cs
@@ -0,0 +1,57 @@
+﻿using ComputeSharp;
+using Microsoft.ML.OnnxRuntime;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using TerraFX.Interop.Windows;
+using YoloDotNet.Core;
+using YoloDotNet.Extensions;
+
+namespace YoloDotNet.GPU.Core
+{
+    [SupportedOSPlatform("windows")]
+    public class YoloCoreDirectX : YoloCoreGPUBase
+    {
+        public YoloCoreDirectX(string onnxModel, bool useCuda, bool allocateGpuMemory, int gpuId) : base(onnxModel, useCuda, allocateGpuMemory, gpuId)
+        {
+
+        }
+
+        private readonly object _inferenceLock = new();
+/*
+        public override IDisposableReadOnlyCollection<OrtValue> Run(SKImage image)
+        { 
+            // resize image
+            
+
+            var tensorArrayBuffer = customSizeFloatPool.Rent(minimumLength: _tensorBufferSize);
+
+            try
+            {
+                lock (_inferenceLock)
+                {
+                    // get densetensor
+
+                    var tensorPixels = resizedImage.NormalizePixelsToTensor(OnnxModel.InputShape, _tensorBufferSize, tensorArrayBuffer);
+
+                    using var inputOrtValue = OrtValue.CreateTensorValueFromMemory(OrtMemoryInfo.DefaultInstance, tensorPixels.Buffer, OnnxModel.InputShape);
+
+                    var inputNames = new Dictionary<string, OrtValue>
+                    {
+                        { OnnxModel.InputName, inputOrtValue }
+                    };
+
+                    return _session.Run(_runOptions, inputNames, OnnxModel.OutputNames);
+                }
+            }
+            finally
+            {
+                customSizeFloatPool.Return(tensorArrayBuffer, true);
+            }
+        }*/
+    }
+}
---
+++ DirectX/Modules/V8/ObjectDetectionDirectXModuleV8.cs
@@ -0,0 +1,38 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoloDotNet.Core;
+using YoloDotNet.GPU.Core;
+using YoloDotNet.Models;
+using YoloDotNet.Modules.Interfaces;
+using YoloDotNet.Modules.V8;
+
+namespace YoloDotNet.GPU.Modules.V8
+{
+    public class ObjectDetectionDirectXModuleV8(YoloCoreDirectX yoloCore) : IObjectDetectionModule
+    {
+        public OnnxModel OnnxModel => yoloCore.OnnxModel;
+
+        public event EventHandler VideoStatusEvent;
+        public event EventHandler VideoProgressEvent;
+        public event EventHandler VideoCompleteEvent;
+
+        public void Dispose()
+        {
+            yoloCore?.Dispose();
+        }
+
+        public List<ObjectDetection> ProcessImage(SKImage image, double confidence, double pixelConfidence, double iou)
+        {
+            yoloCore
+        }
+
+        public Dictionary<int, List<ObjectDetection>> ProcessVideo(VideoOptions options, double confidence, double pixelConfidence, double iou)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
