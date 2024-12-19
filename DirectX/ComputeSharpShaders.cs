56437bb000e7d13c2b7c4a0a1c42bcab5131925d
Haves Irfan <20160532+ha-ves@users.noreply.github.com>
2024-12-20 01:32:43 +0900

    Create ComputeSharpShaders.cs

---
+++ DirectX/ComputeSharpShaders.cs
@@ -0,0 +1,23 @@
+﻿using ComputeSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoloDotNet.GPU.DirectX
+{
+    public static partial class ComputeSharpShaders
+    {
+
+        [ThreadGroupSize(DefaultThreadGroupSizes.XY)]
+        [GeneratedComputeShaderDescriptor]
+        public readonly partial struct Rgba32ToNormalizedTensor(ReadWriteTexture2D<Rgba32> src, ReadWriteBuffer<float> tensor) : IComputeShader
+        {
+            public void Execute()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
