7302d53ab2f0a4b5c5dce9737cb8c6c28b1476b6
Haves Irfan <20160532+ha-ves@users.noreply.github.com>
2025-01-22 18:43:42 +0900

    integrate linear image scaling to shader.

--- DirectX/ComputeSharpShaders.cs
+++ DirectX/ComputeSharpShaders.cs
@@ -2,45 +2,214 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using YoloDotNet.Models;
 
 namespace YoloDotNet.GPU.DirectX
 {
     public static partial class ComputeSharpShaders
     {
-        [ThreadGroupSize(DefaultThreadGroupSizes.XY)]
+        /// <summary>
+        /// Resizing algorithm is Linear.
+        /// </summary>
+        [ThreadGroupSize(DefaultThreadGroupSizes.XYZ)]
         [GeneratedComputeShaderDescriptor]
-        public readonly partial struct NormalizeRgbaToSingleTensor( 
-            ReadOnlyTexture2D<int> RgbaData,
-            ReadWriteTexture3D<float> DenseTensor,
-            float NormalizeBy = 255f)
-            : IComputeShader
+        public readonly partial struct NormalizePixelsToTensorSingle_3D : IComputeShader
         {
-            readonly float normMul = 1f / NormalizeBy;
+            /// <summary>
+            /// The constructor is run on Host (managed).
+            /// </summary>
+            /// <param name="image">source image.</param>
+            /// <param name="tensor">input tensor.</param>
+            /// <param name="keepAspect"></param>
+            /// <param name="emptyResizePixelFill"></param>
+            public NormalizePixelsToTensorSingle_3D(
+                ReadOnlyTexture2D<Bgra32, Float4> image,
+                ReadWriteTexture3D<float> tensor,
+                bool keepAspect = true,
+                Float3 emptyResizePixelFill = default
+            )
+            {
+                InputPixels = image;
+                InputTensor = tensor;
+
+                if (!keepAspect)
+                {
+                    KeepAspect = 0;
+                    return;
+                }
+                else
+                {
+                    //KeepAspect = Random.Shared.Next();
+                    KeepAspect = 1;
+                }
+
+                int modelWidth = tensor.Width;
+                int modelHeight = tensor.Height;
+                int width = image.Width;
+                int height = image.Height;
+
+                // Calculate the new image size based on the aspect ratio
+                float scaleFactor = Math.Min((float)modelWidth / width, (float)modelHeight / height);
+                int newWidth = (int)Math.Floor(width * scaleFactor);
+                int newHeight = (int)Math.Floor(height * scaleFactor);
+
+                // Calculate the destination rectangle within the model dimensions
+                int x = (modelWidth - newWidth) / 2;
+                int y = (modelHeight - newHeight) / 2;
+
+                resizeRect = new ResizeRect(
+                    new(1f / newWidth, 1f / newHeight),
+                    new(x, y),
+                    new(x + newWidth, y + newHeight),
+                    emptyResizePixelFill);
+            }
+
+            public struct ResizeRect(Float2 wh, Int2 leftTop, Int2 botRight, Float3 empFiller)
+            {
+                public readonly Float2 NormalizedWidthHeight = wh;
+                public readonly Int2 LeftTop = leftTop;
+                public readonly Int2 BottomRight = botRight;
+                public readonly Float3 EmptyPixelFiller = empFiller;
+            }
+
+            readonly ReadOnlyTexture2D<Bgra32, Float4> InputPixels;
+            readonly ReadWriteTexture3D<float> InputTensor;
+            readonly int KeepAspect = 0;
+
+            readonly ResizeRect resizeRect;
 
             public void Execute()
             {
-                // Read the 32-bit ARGB pixel value
-                var px = RgbaData[ThreadIds.XY];
-                var pixel = new Float3(
-                    px & 0xFF,           // Red
-                    (px >> 8) & 0xFF,    // Green
-                    (px >> 16) & 0xFF    // Blue
-                    //(px >> 24) & 0xFF   // Alpha
-                );
-
-                // Skip processing if RGB channels are zero (regardless of A-channel opacity)
-                if (px << 8 == 0)
+                //if(Hlsl.IntToBool(KeepAspect))
+                if (KeepAspect == 0)
                 {
+                    // TODO: another texture2D to save sampled texture?
+                    var px = InputPixels.Sample(ThreadIds.Normalized.XY);
+                    InputTensor[ThreadIds.XYZ] = px[ThreadIds.Z];
+                }
+                else
+                {
+                    if (Hlsl.Any(
+                        new Bool2x2(ThreadIds.XY <= resizeRect.LeftTop,
+                                        ThreadIds.XY >= resizeRect.BottomRight)
+                    ))
+                    {
+                        InputTensor[ThreadIds.XYZ] = resizeRect.EmptyPixelFiller[ThreadIds.Z];
+                    }
+                    else
+                    {
+                        // TODO: another texture2D to save sampled texture?
+                        var start = (ThreadIds.XY - resizeRect.LeftTop) * resizeRect.NormalizedWidthHeight;
+                        var px = InputPixels.Sample(start);
+                        InputTensor[ThreadIds.XYZ] = px[ThreadIds.Z];
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Resizing algorithm is Linear.
+        /// </summary>
+        [ThreadGroupSize(DefaultThreadGroupSizes.XY)]
+        [GeneratedComputeShaderDescriptor]
+        public readonly partial struct NormalizePixelsToTensorSingle_2D : IComputeShader
+        {
+            /// <summary>
+            /// The constructor is run on Host (managed).
+            /// </summary>
+            /// <param name="image">source image.</param>
+            /// <param name="tensor">input tensor.</param>
+            /// <param name="keepAspect"></param>
+            /// <param name="emptyResizePixelFill"></param>
+            public NormalizePixelsToTensorSingle_2D(
+                ReadOnlyTexture2D<Bgra32, Float4> image,
+                ReadWriteTexture3D<float> tensor,
+                bool keepAspect = true,
+                Float3 emptyResizePixelFill = default
+            )
+            {
+                InputPixels = image;
+                InputTensor = tensor;
+
+                if (!keepAspect)
+                {
+                    KeepAspect = 0;
                     return;
                 }
+                else
+                {
+                    //KeepAspect = Random.Shared.Next();
+                    KeepAspect = 1;
+                }
 
-                var N_pixel = Hlsl.Mul(pixel.RGB, normMul);
+                int modelWidth = tensor.Width;
+                int modelHeight = tensor.Height;
+                int width = image.Width;
+                int height = image.Height;
 
-                DenseTensor[new(ThreadIds.XY, 0)] = N_pixel.R;
-                DenseTensor[new(ThreadIds.XY, 1)] = N_pixel.G;
-                DenseTensor[new(ThreadIds.XY, 2)] = N_pixel.B;
+                // Calculate the new image size based on the aspect ratio
+                float scaleFactor = Math.Min((float)modelWidth / width, (float)modelHeight / height);
+                int newWidth = (int)Math.Floor(width * scaleFactor);
+                int newHeight = (int)Math.Floor(height * scaleFactor);
+
+                // Calculate the destination rectangle within the model dimensions
+                int x = (modelWidth - newWidth) / 2;
+                int y = (modelHeight - newHeight) / 2;
+
+                resizeRect = new ResizeRect(
+                    new(1f / newWidth, 1f / newHeight),
+                    new(x, y),
+                    new(x + newWidth, y + newHeight),
+                    emptyResizePixelFill);
+            }
+
+            public struct ResizeRect(Float2 wh, Int2 leftTop, Int2 botRight, Float3 empFiller)
+            {
+                public readonly Float2 NormalizedWidthHeight = wh;
+                public readonly Int2 LeftTop = leftTop;
+                public readonly Int2 BottomRight = botRight;
+                public readonly Float3 EmptyPixelFiller = empFiller;
+            }
+
+            readonly ReadOnlyTexture2D<Bgra32, Float4> InputPixels;
+            readonly ReadWriteTexture3D<float> InputTensor;
+            readonly int KeepAspect = 0;
+
+            readonly ResizeRect resizeRect;
+
+            public void Execute()
+            {
+                //if(Hlsl.IntToBool(KeepAspect))
+                if (KeepAspect == 0)
+                {
+                    var px = InputPixels.Sample(ThreadIds.Normalized.XY);
+                    InputTensor[new(ThreadIds.XY, 0)] = px.R;
+                    InputTensor[new(ThreadIds.XY, 1)] = px.G;
+                    InputTensor[new(ThreadIds.XY, 2)] = px.B;
+                }
+                else
+                {
+                    if (Hlsl.Any(
+                        new Bool2x2(ThreadIds.XY <= resizeRect.LeftTop,
+                                        ThreadIds.XY >= resizeRect.BottomRight)
+                    ))
+                    {
+                        InputTensor[new(ThreadIds.XY, 0)] = resizeRect.EmptyPixelFiller.R;
+                        InputTensor[new(ThreadIds.XY, 1)] = resizeRect.EmptyPixelFiller.G;
+                        InputTensor[new(ThreadIds.XY, 2)] = resizeRect.EmptyPixelFiller.B;
+                    }
+                    else
+                    {
+                        var start = (ThreadIds.XY - resizeRect.LeftTop) * resizeRect.NormalizedWidthHeight;
+                        var px = InputPixels.Sample(start);
+                        InputTensor[new(ThreadIds.XY, 0)] = px.R;
+                        InputTensor[new(ThreadIds.XY, 1)] = px.G;
+                        InputTensor[new(ThreadIds.XY, 2)] = px.B;
+                    }
+                }
             }
         }
     }
